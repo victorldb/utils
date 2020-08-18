a6d0886c42e53cecce4aa682db928abe88450b44
victor <ldb8002@gmail.com>
2020-08-18 15:09:55 +0800

    update

--- net.go
+++ net.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"net"
+	"strings"
 	"time"
 )
 
@@ -15,4 +16,21 @@ func TimeoutDialer(cTimeout time.Duration, rwTimeout time.Duration) func(net, ad
 		err = conn.SetDeadline(time.Now().Add(rwTimeout))
 		return conn, err
 	}
+}
+
+// CheckTCPAddress 检查TCP地址
+func CheckTCPAddress(s string) (res string, err error) {
+	ind1 := strings.Index(s, ".")
+	if ind1 == -1 {
+		if ind2 := strings.Index(s, ":"); ind2 == -1 {
+			s = ":" + s
+		}
+	}
+
+	// 不使用net.SplitHostPort原因是没有对IP和端口合法性进行检测
+	_, err = net.ResolveTCPAddr("tcp", s)
+	if err != nil {
+		return res, err
+	}
+	return s, nil
 }
