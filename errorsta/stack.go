0cf21d88dc2a834c1e7fa14994927e8d58e70933
victor <ldb8002@gmail.com>
2020-11-17 14:49:48 +0800

    update

--- errorsta/stack.go
+++ errorsta/stack.go
@@ -8,6 +8,9 @@ import (
 
 // LastStackError --
 func LastStackError(err error) error {
+	if err == nil {
+		return nil
+	}
 	pc, file, line, ok := runtime.Caller(1)
 	if ok {
 		err = fmt.Errorf("%+w\n%s:%d %s", err, filepath.Base(file), line, runtime.FuncForPC(pc).Name())
